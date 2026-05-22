2ccf62dcd3dcf46f9a48077abc9826ba949da70a
EirikErKulere <eirikgf08@gmail.com>
2026-05-23 00:20:39 +0200

    Fix opengl-test-4

--- Formula/opengl-test-4.rb
+++ Formula/opengl-test-4.rb
@@ -1,43 +1,21 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://docs.brew.sh/rubydoc/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class OpenglTest4 < Formula
-  desc "Prosjekt for å teste ting i OpenGL."
+  desc "Prosjekt for å teste ting i OpenGL"
   homepage "https://github.com/EirikErKulere/OpenGL_Test_4"
   url "https://github.com/EirikErKulere/OpenGL_Test_4/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "da0bbe5655ad038639f12cad38e481d877e197c2f848f61ef4bd7cc545b0527b"
   license ""
 
   depends_on "cmake" => :build
-  depends_on "glfw"
   depends_on "assimp"
-
-  # Additional dependency
-  # resource "" do
-  #   url ""
-  #   sha256 ""
-  # end
+  depends_on "glfw"
 
   def install
-    # Remove unrecognized options if they cause configure to fail
-    # https://docs.brew.sh/rubydoc/Formula.html#std_configure_args-instance_method
-    # system "./configure", "--disable-silent-rules", *std_configure_args
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
 
   test do
-    assert_predicate bin/"OpenGL_Test_4", :exist?
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test opengl-test-4`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    # system "false"
+    assert_path_exists bin/"OpenGL_Test_4"
   end
 end
