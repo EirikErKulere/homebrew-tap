e3fe0934209427b09b3be8848d2ba5d5a6324db0
Eirik <eirikgf08@gmail.com>
2026-09-02 23:06:52 +0200

    Update username in opengl-test-4.rb

--- Formula/opengl-test-4.rb
+++ Formula/opengl-test-4.rb
@@ -1,7 +1,7 @@
 class OpenglTest4 < Formula
   desc "Prosjekt for å teste ting i OpenGL"
-  homepage "https://github.com/EirikErKulere/OpenGL_Test_4"
-  url "https://github.com/EirikErKulere/OpenGL_Test_4/archive/refs/tags/v0.1.0.tar.gz"
+  homepage "https://github.com/E1fl4/OpenGL_Test_4"
+  url "https://github.com/E1fl4/OpenGL_Test_4/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "da0bbe5655ad038639f12cad38e481d877e197c2f848f61ef4bd7cc545b0527b"
   license ""
 
